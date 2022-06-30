962347d265143e928d447be570e4e0c17705032f
Kacper Bukowski <kacper.bukowski@onwelo.com>
2022-06-30 19:41:06 +0200

    fixed many errors

---
+++ first attempt/baseline.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+import logging
+import seaborn as sns
+
+class Baseline(tf.keras.Model):
+  def __init__(self, label_index=None):
+    super().__init__()
+    self.label_index = label_index
+
+  def call(self, inputs):
+    if self.label_index is None:
+      return inputs
+    result = inputs[:, :, self.label_index]
+    return result[:, :, tf.newaxis]
